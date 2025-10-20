fcb11cec52374d0ee36908e352a9e03ce8425147
Rob Timpe <rtimpe@openteams.com>
2025-10-20 20:53:11 +0000

    pixi

--- pytorch-build.sh
+++ pytorch-build.sh
@@ -3,9 +3,9 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-eval "$(conda shell.bash hook)"
-conda activate ${PYTORCH_CONDA_ENV:=pytorch-dev$PYTORCH_BUILD_SUFFIX}
-cd ${PYTORCH_BUILD_DIRECTORY:=~/git$PYTORCH_BUILD_SUFFIX}/pytorch
+# eval "$(conda shell.bash hook)"
+# conda activate ${PYTORCH_CONDA_ENV:=pytorch-dev$PYTORCH_BUILD_SUFFIX}
+# cd ${PYTORCH_BUILD_DIRECTORY:=~/git$PYTORCH_BUILD_SUFFIX}/pytorch
 
 source $SCRIPT_DIR/torch-common.sh
 
